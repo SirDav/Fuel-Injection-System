2b1a301c954263506e58b7ae637aa2045fddd577
SirDav <sirdav.contact@gmail.com>
2018-05-17 00:23:06 -0300

    Adding button with debounce and Display Screen Rotation

--- EngineControlUnit/EngineControlUnit.ino
+++ EngineControlUnit/EngineControlUnit.ino
@@ -41,13 +41,21 @@ PROCESSING EQUATIONS:
 // Setup Variables
 const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 12;    //LCD interface pinout
 int analog_temp = 0;                                            // A0 analog input for TEMP sensor
+const int buttonPin = 2;                                        // External button for menu
+const int ledPin =  13;                                         // the number of the LED pin
 
 // Global Variables
+int lastButtonState = LOW;
+int buttonState;
+unsigned long lastDebounceTime = 0;                             // Last Time the Key was Toggled
+unsigned long debounceDelay = 50;                               // Debounce Time; Increase if the Input Flickers
+int display_message = 0;                                        // Display Screen in use (Value to Print)
 int temp = 273;                                                 // Default Temperature: 273K = 0C
 int N = 4;                                                      // Number of Cilinders
 int P = 1;                                                      // Default Atmospheric Pressure
 float R = 0.18;                                                      // Default Gasoline Molecular Weight
 int V = 1;                                                      // Default Air Mass Volume
+float MAF = 0;
 int Inj_output = 3;                                             // Output pin for Injector
 
 // Initialization
@@ -55,6 +63,42 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);                      // LCD setup
 
 // Functions
 
+void display_rotate(void)
+{
+  if (display_message < 2)
+    {display_message++;}
+  else
+    {display_message = 0;}
+  Serial.println (display_message);
+}
+
+void display_print()
+{
+  lcd.clear();
+  switch(display_message){
+    case 0:
+      lcd.setCursor(0, 0);
+      lcd.print ("MAF");
+      lcd.setCursor(0, 1);
+      lcd.print(MAF,4);
+      break;
+    case 1:
+      lcd.setCursor(0, 0);
+      lcd.print ("Temperature");
+      lcd.setCursor(0, 1);
+      lcd.print(String((temp-273))+String(" C"));
+      break;
+    case 2:
+      lcd.setCursor(0, 0);
+      lcd.print ("Pressure");
+      lcd.setCursor(0, 1);
+      lcd.print(P);
+      break;
+    default:
+      break;
+  }
+  
+}
 float A_calc (int P, int V, int T)
 {
   float result;
@@ -65,8 +109,10 @@ float A_calc (int P, int V, int T)
 // Setup Routine
 void setup() 
 {
+  pinMode(buttonPin, INPUT);                                   // Initialize the pushbutton pin as an input
+  pinMode(ledPin, OUTPUT);
   Serial.begin(9600);                                          // Initialize Serial Port - 9600 baudrate.
-  lcd.begin(16, 2);                                            // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);                                            // Set up the LCD's number of columns and rows
   lcd.print ("Initialized");
   Serial.println("Initializing...");
 }
@@ -75,10 +121,22 @@ void setup()
 void loop()
 {
   temp = analogRead(analog_temp);                              // Read TEMP from A0 pin
-  float MAF=(float)A_calc(P, V, temp*5);                       // Calculate Air/Fuel Mixture (n), temp is converted to C based on ADC counts
-  lcd.clear();
-  lcd.print(MAF,4);
-  Serial.println(MAF,4);
-  analogWrite(Inj_output, temp/4);
+  MAF=(float)A_calc(P, V, temp*5);                             // Calculate Air/Fuel Mixture (n), temp is converted to C based on ADC counts
+  display_print();
+  //analogWrite(Inj_output, temp/4);
+  int reading = digitalRead(buttonPin);
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {          // Check if key was pressed longer than DebounceTime to remove flickers
+    if (reading != buttonState) {
+      buttonState = reading;
+      if (buttonState == HIGH) {
+        display_rotate();
+      }
+    }
+  }
+  lastButtonState = reading;
   delay(100); 
 }
+
